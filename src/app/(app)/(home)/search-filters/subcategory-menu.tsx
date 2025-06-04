b0f387f975dae2c28fdde98990860282ca82da7f
Jefferson R. Shibuya <jeffersonshibuya@yahoo.com.br>
2025-06-04 13:11:45 -0300

    05-search-filters

---
+++ src/app/(app)/(home)/search-filters/subcategory-menu.tsx
@@ -0,0 +1,47 @@
+import { Category } from "@/payload-types";
+import Link from "next/link";
+
+interface SubcategoryMenuProps {
+  category: Category;
+  isOpen: boolean;
+  position: { top: number; left: number };
+}
+
+export const SubcategoryMenu = ({
+  category,
+  isOpen,
+  position,
+}: SubcategoryMenuProps) => {
+  if (
+    !isOpen ||
+    !category.subcategories ||
+    category.subcategories.length === 0
+  ) {
+    return null;
+  }
+
+  const backgroundColor = category.color || "#f5f5f5";
+
+  return (
+    <div className="absolute z-100 top-[100%] left-0">
+      {/* Invisible bridge to maintin hover */}
+      <div className="h-3 w-60" />
+      <div
+        style={{ backgroundColor: backgroundColor }}
+        className="w-60 text-black rounded-md overflow-hidden border shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px]"
+      >
+        <div>
+          {category.subcategories?.map((subcategory: Category) => (
+            <Link
+              href={"/"}
+              key={subcategory.id}
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center underline font-medium"
+            >
+              {subcategory.name}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
