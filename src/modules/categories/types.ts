73040ba0860730cf4f7c45a57125975bf03cf924
Jefferson R. Shibuya <jeffersonshibuya@yahoo.com.br>
2025-06-20 13:43:18 -0300

    trpc integration

--- src/modules/categories/types.ts
+++ src/modules/categories/types.ts
@@ -1,6 +1,6 @@
-import { AppRouter } from "@/trpc/routers/_app";
-import type { inferRouterOutputs } from "@trpc/server";
+import type { AppRouter } from "@/trpc/routers/_app";
+import { inferRouterOutputs } from "@trpc/server";
 
 export type CategoriesGetManyOutput =
   inferRouterOutputs<AppRouter>["categories"]["getMany"];
-export type CategoriesGetManyOutputSIngle = CategoriesGetManyOutput[0];
+export type CategoriesGetManyOutputSingle = CategoriesGetManyOutput[0];
